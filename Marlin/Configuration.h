93bbfcd1fefd12cb38e34b760b7bf80ef2c8730c
Hesham El-sewify (LehKeda) <computer.boy71@yahoo.com>
2024-01-24 16:45:03 +0200

    Change leveling preheating temps

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1890,8 +1890,8 @@
  */
 #define PREHEAT_BEFORE_LEVELING
 #if ENABLED(PREHEAT_BEFORE_LEVELING)
-  #define LEVELING_NOZZLE_TEMP 170   // (°C) Only applies to E0 at this time
-  #define LEVELING_BED_TEMP     70
+  #define LEVELING_NOZZLE_TEMP 200   // (°C) Only applies to E0 at this time
+  #define LEVELING_BED_TEMP     95
 #endif
 
 /**
