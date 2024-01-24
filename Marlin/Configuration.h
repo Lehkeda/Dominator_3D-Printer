b573b7c1795d0bd878518573d3e98f5146a78dc3
Hesham El-sewify (LehKeda) <computer.boy71@yahoo.com>
2024-01-24 16:39:19 +0200

    Update PID values

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -668,9 +668,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp  33.32
-    #define DEFAULT_Ki   3.18
-    #define DEFAULT_Kd 87.34
+    #define DEFAULT_Kp  33.59
+    #define DEFAULT_Ki   3.23
+    #define DEFAULT_Kd 87.36
   #endif
 #endif
 
